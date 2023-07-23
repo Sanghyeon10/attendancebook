53890962f5920f2626e8109d016a31ad98afd358
Sanghyeon10 <130223997+Sanghyeon10@users.noreply.github.com>
2023-07-23 19:56:08 +0900

    새해에 양식만들때 쓰는 코드

---
+++ happynewyear.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import datetime
+import making
+import re
+
+all_group= making.all_group()
+
+# 출석 정보가 저장된 파일 경로를 입력합니다.
+attendance_file_path = 'attendance.txt' #첫 엑셀표 양식만들때 쓰는거라 같은 텍스트파일을 활용함.
+
+# 목장 출석 정보를 저장할 딕셔너리를 생성합니다.
+attendance_dict = {}
+
+# 출석 정보 파일을 읽어서 딕셔너리에 저장합니다.
+# 주의사항, 목장 내용인데 사이가 space바여야지, tab(\t)으로 구분되어있으면 오류남
+with open(attendance_file_path, 'r', encoding='utf-8') as f:
+    for line in f:
+        # 한 줄씩 읽어서 공백 , .을 기준으로 분리합니다.(정규 표현식 활용했음)
+        # fields = line.strip().split()
+        fields = [line for line in re.split('\s|,|\.', line) if line]
+
+        # 목장 이름, 출석자 이름1, 출석자 이름2, ...으로 분리합니다.
+        farm_name, *attendees = fields
+
+        # 딕셔너리에 목장 이름을 키로, 출석자 이름 리스트를 값으로 저장합니다.
+        attendance_dict[farm_name] = attendees
+
+# 각 목장의 출석 정보 리스트를 출력합니다.
+for farm_name, attendees in attendance_dict.items():
+    print(f'{farm_name} 목장 출석자: {attendees}', '인원수:' ,len(attendees))
+
+
+# 엑셀표 작성해서 출력함
+for i in range(len(all_group)):
+    df = pd.DataFrame(columns=attendance_dict[all_group[i]], index=making.index())
+    df.rename_axis('날짜\이름',inplace=True)
+
+    df.to_excel("{}.xlsx".format(all_group[i]),  index=True )  # 5-1식으로 출력
