2d8f1a90584d397be4a80a3cd33d9c97b55ecdae
Sanghyeon10 <130223997+Sanghyeon10@users.noreply.github.com>
2023-11-05 17:15:38 +0900

    Add files via upload

--- for test.py
+++ for test.py
@@ -1,4 +1,18 @@
-import pandas as pd
+from itertools import *
 
-df = pd.read_excel(r'C:\Users\User\Downloads\스태디키워드_뜻.xlsx')
-print(df)
+arr= [list(map(int,input()) for _ in range(2))]
+print(arr)
+N, K= map(int,input().split())
+givennubmer=list()
+
+conbination = list(combinations(range(N), K))
+givennubmerbest=0
+
+givennubmer= str(givennubmer)
+for i in range(len(conbination)):
+    for m in range(K):
+        givennubmer= givennubmer[:conbination[m]]+" "+givennubmer[conbination[m]+1:]
+
+    givennubmer=int(givennubmer.strip())
+    givennubmerbest=max(givennubmer,givennubmerbest)
+
